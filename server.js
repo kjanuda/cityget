2631b31f30cff3900d941167be23b24bd1533342
Januda J Kodithuwakku <113584734+kjanuda@users.noreply.github.com>
2025-11-17 20:43:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001','https://urbanflowsl.netlify.app/'], // Next.js dev server
+  origin: ['http://localhost:3000', 'http://localhost:3001','https://urbanflowsl.netlify.app'], // Next.js dev server
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
